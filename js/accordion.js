48290c9e95f9b542dcda817a1d64848e43bbc28c
Иван Прилипко <prilipko.ia@academia-moscow.ru>
2021-09-22 10:12:14 +0300

    fix accordion

--- js/accordion.js
+++ js/accordion.js
@@ -2,7 +2,7 @@ const lists = document.querySelectorAll('.feature-sub');
 const btns = document.querySelectorAll('.feature__link');
 
 
-btns.forEach((btn, index) => {
+btns.forEach((btn) => {
     btn.addEventListener('click', () => {
         btns.forEach((btnItem, idx) => {
 
